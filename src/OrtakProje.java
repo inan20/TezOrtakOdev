c273ef24995a2607c840b7ef58e600c205e85288
inan20 <ahmetinanyunefa@gmail.com>
2022-08-20 15:15:22 +0300

    proje

---
+++ src/OrtakProje.java
@@ -0,0 +1,25 @@
+public class OrtakProje {
+    public static <Öğrenci> void main(String[] args) {
+        //Team 09 olarak yeni spariş aldığımız Yıldız Koleji ortak proje yapacağiz
+        // Aşagidaki ilgili bölümleri görev dağilimi yaparak dolduralım*/
+         ÖğrenciKayıtEkleme(); /* görevli kişi ...  */
+         ÖğrenciKayıtSilme();  /* görevli kişi....   */
+         ÖğretmenKayıtEkleme(); /* görevli kişi ...  */
+    }
+
+    private static void ÖğretmenKayıtEkleme() {
+        /*Öğrenci Eklemek için gerekli işlemleri yapınız.*/
+
+
+    }
+
+    private static void ÖğrenciKayıtSilme() {
+        /*Öğrenci kayıt silme işlemlerini yapınız*/
+    }
+
+    private static void ÖğrenciKayıtEkleme() {
+        /*Öğretmen Kayıt Eklem  işlemlerini yapınız*/
+
+
+    }
+}
