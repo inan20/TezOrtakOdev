b8e4a21412414d26ca3eeb027728ac843410d1e3
inan20 <ahmetinanyunefa@gmail.com>
2022-08-20 08:24:57 +0300

    m2

--- src/C01_Gitİgnore.java
+++ src/C01_Gitİgnore.java
@@ -4,5 +4,17 @@ public class C01_Gitİgnore {
 
     public static void main(String[] args) {
         System.out.println("gitignore deneme 1");
+        System.out.println("gitignore deneme 2");
+
+
+
+
+
+
+
+
+
+
+
     }
 }
