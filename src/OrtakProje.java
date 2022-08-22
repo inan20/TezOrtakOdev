b5e3b138a86f8df4e766c5c8f4ec7d3e66f45bbf
Ahmet İnan <110977687+inan20@users.noreply.github.com>
2022-08-22 08:39:49 +0300

    Update OrtakProje.java

--- src/OrtakProje.java
+++ src/OrtakProje.java
@@ -2,9 +2,11 @@ public class OrtakProje {
     public static  void main(String[] args) {
         //Team 09 olarak yeni spariş aldığımız Yıldız Koleji ortak proje yapacağiz
         // Aşagidaki ilgili bölümleri görev dağilimi yaparak dolduralım*/
-         ÖğrenciKayıtEkleme();  görevli kişi ..
-         ÖğrenciKayıtSilme();   görevli kişi..
-         ÖğretmenKayıtEkleme();  görevli kişi .
+        
+        
+          ÖğretmenKayıtEkleme();  görevli kişi   14.  Satır
+          ÖğrenciKayıtSilme();   görevli kişi    20.  Satır
+          ÖğrenciKayıtEkleme();  görevli kişi    27.  Satır 
     }
 
     private static void ÖğretmenKayıtEkleme() {
@@ -15,11 +17,16 @@ public class OrtakProje {
 
     private static void ÖğrenciKayıtSilme() {
         /*Öğrenci kayıt silme işlemlerini yapınız*/
+   
+    
+    
     }
 
     private static void ÖğrenciKayıtEkleme() {
         /*Öğretmen Kayıt Eklem  işlemlerini yapınız*/
 
 
+        
+        
     }
 }
